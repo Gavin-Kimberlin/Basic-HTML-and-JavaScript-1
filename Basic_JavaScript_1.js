7d90bfc5567a3d1432f867a179a2a0e03b247acd
Gavin Kimberlin <guviner@gmail.com>
2019-12-30 20:13:38 -0800

    create a couple pages using newly learned material.

---
+++ Basic_JavaScript_1.js
@@ -0,0 +1,6 @@
+function myFirstFunction() {
+  var String = 'Kiss me, I\'m Irish!';
+  var result = String.fontcolor('green');
+  document.getElementById('irish').innerHTML = result;
+  console.log(result);
+};
